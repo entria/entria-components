b7340f3e65ed6c539c1789946e251aaaa19e6b0f
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-06-21 14:38:39 -0300

    Remove injectTapEventPlugin frm ThemeProvider

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Theme from './Theme';
 
-injectTapEventPlugin();
-
-const ThemeProvider = ({ children }) => (
+const ThemeProvider = ({ children }) =>
   <MuiThemeProvider muiTheme={Theme}>
     {children}
-  </MuiThemeProvider>
-);
+  </MuiThemeProvider>;
 
 export default ThemeProvider;
--- storybook/config.js
+++ storybook/config.js
@@ -2,24 +2,27 @@ import React from 'react';
 import { addDecorator, configure } from '@kadira/storybook';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import { ThemeProvider } from '../src';
 import store from './store';
 
+injectTapEventPlugin();
+
 const req = require.context('./stories', true, /\.story\.js$/);
 
-addDecorator(story => (
+addDecorator(story =>
   <Provider store={store}>
     <MemoryRouter initialEntries={['/']}>
       <ThemeProvider>
         {story()}
       </ThemeProvider>
     </MemoryRouter>
-  </Provider>
-));
+  </Provider>,
+);
 
-function loadStories () {
-  req.keys().forEach((filename) => req(filename));
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
