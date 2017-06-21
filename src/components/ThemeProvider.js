2e35a736499ffebb975b09d47791f83c700f684e
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-06-21 13:33:25 -0300

    Add injectTapEventPlugin

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Theme from './Theme';
+
+injectTapEventPlugin();
 
 const ThemeProvider = ({ children }) => (
   <MuiThemeProvider muiTheme={Theme}>
