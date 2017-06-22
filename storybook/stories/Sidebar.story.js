f11a78e65e0ca016764b7da8c6cf09b4de5d4fd0
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-06-22 10:35:27 -0300

    Add Content to Sidebar stories

--- storybook/stories/Sidebar.story.js
+++ storybook/stories/Sidebar.story.js
@@ -1,79 +1,77 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import loremIpsum from 'lorem-ipsum';
 
 import FlatButton from 'material-ui/FlatButton';
-import { Sidebar } from '../../src';
+import { Sidebar, Content } from '../../src';
 
 const stories = storiesOf('Sidebar', module);
 
-stories.add('default', () => (
+stories.add('default', () =>
   <Sidebar style={styles.sidebar}>
     <Sidebar.Menu>
-      <Sidebar.Item link='#'>Link 1</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 2</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 3</Sidebar.Item>
+      <Sidebar.Item link="#">Link 1</Sidebar.Item>
+      <Sidebar.Item link="#">Link 2</Sidebar.Item>
+      <Sidebar.Item link="#">Link 3</Sidebar.Item>
     </Sidebar.Menu>
-  </Sidebar>
-));
+  </Sidebar>,
+);
 
-stories.add('with header', () => (
-  <Sidebar style={styles.sidebar}>
-    <Sidebar.Header>Header</Sidebar.Header>
-    <Sidebar.Menu>
-      <Sidebar.Item link='#'>Link 1</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 2</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 3</Sidebar.Item>
-    </Sidebar.Menu>
-  </Sidebar>
-));
-
-stories.add('with many menus', () => (
+stories.add('with header', () =>
   <Sidebar style={styles.sidebar}>
     <Sidebar.Header>Header</Sidebar.Header>
     <Sidebar.Menu>
-      <Sidebar.Item link='#'>Link 1</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 2</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 3</Sidebar.Item>
+      <Sidebar.Item link="#">Link 1</Sidebar.Item>
+      <Sidebar.Item link="#">Link 2</Sidebar.Item>
+      <Sidebar.Item link="#">Link 3</Sidebar.Item>
+    </Sidebar.Menu>
+  </Sidebar>,
+);
+
+stories.add('with many menus', () =>
+  <Sidebar style={styles.sidebar}>
+    <Sidebar.Header>Header</Sidebar.Header>
+    <Sidebar.Menu>
+      <Sidebar.Item link="#">Link 1</Sidebar.Item>
+      <Sidebar.Item link="#">Link 2</Sidebar.Item>
+      <Sidebar.Item link="#">Link 3</Sidebar.Item>
     </Sidebar.Menu>
     <Sidebar.Menu>
-      <Sidebar.Item link='#'>Link 4</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 5</Sidebar.Item>
-      <Sidebar.Item link='#'>Link 6</Sidebar.Item>
+      <Sidebar.Item link="#">Link 4</Sidebar.Item>
+      <Sidebar.Item link="#">Link 5</Sidebar.Item>
+      <Sidebar.Item link="#">Link 6</Sidebar.Item>
     </Sidebar.Menu>
-  </Sidebar>
-));
+  </Sidebar>,
+);
 
-stories.add('with toggler', () => (
-  <div>
+stories.add('with toggler', () =>
+  <Content style={styles.content}>
     <Sidebar style={styles.sidebar}>
       <Sidebar.Header>Header</Sidebar.Header>
       <Sidebar.Menu>
-        <Sidebar.Item link='#'>Link 1</Sidebar.Item>
-        <Sidebar.Item link='#'>Link 2</Sidebar.Item>
-        <Sidebar.Item link='#'>Link 3</Sidebar.Item>
+        <Sidebar.Item link="#">Link 1</Sidebar.Item>
+        <Sidebar.Item link="#">Link 2</Sidebar.Item>
+        <Sidebar.Item link="#">Link 3</Sidebar.Item>
       </Sidebar.Menu>
       <Sidebar.Menu>
-        <Sidebar.Item link='#'>Link 4</Sidebar.Item>
-        <Sidebar.Item link='#'>Link 5</Sidebar.Item>
-        <Sidebar.Item link='#'>Link 6</Sidebar.Item>
+        <Sidebar.Item link="#">Link 4</Sidebar.Item>
+        <Sidebar.Item link="#">Link 5</Sidebar.Item>
+        <Sidebar.Item link="#">Link 6</Sidebar.Item>
       </Sidebar.Menu>
     </Sidebar>
 
-    <div style={styles.content}>
+    <Content.Body>
       <Sidebar.Toggler>
         <FlatButton label="Toggle" />
       </Sidebar.Toggler>
-    </div>
-  </div>
-));
+    </Content.Body>
+  </Content>,
+);
 
 const styles = {
+  content: {
+    paddingTop: 0,
+  },
   sidebar: {
     top: 0,
   },
-  content: {
-    paddingTop: 20,
-    paddingLeft: 320,
-  }
 };
