e54efbc472e3c25f4144445558c465e3885c8563
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-06-22 11:45:10 -0300

    Add Template component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ export Header from './components/header/Header';
 export Sidebar from './components/sidebar/Sidebar';
 export DocumentTitle from './components/utils/DocumentTitle';
 
+export Template from './components/Template';
+
 export Theme from './components/Theme';
 export ThemeProvider from './components/ThemeProvider';
 
