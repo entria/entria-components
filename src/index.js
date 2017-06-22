a7e806efa08efd9977842b738eb95b416db9ada8
Felippe Rodrigo Puhle <felippe.puhle@gmail.com>
2017-06-22 11:06:44 -0300

    Add DocumentTitle component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export Card from './components/card/Card';
 export Content from './components/content/Content';
 export Header from './components/header/Header';
 export Sidebar from './components/sidebar/Sidebar';
+export DocumentTitle from './components/utils/DocumentTitle';
 
 export Theme from './components/Theme';
 export ThemeProvider from './components/ThemeProvider';
